369d026353300287b6fcfaece3913bdbba730506
Angel Montania <AngelGuillermoMontania@gmail.com>
2022-09-06 22:07:51 -0300

    js basic

---
+++ Javascript/Variables y operadores/Codigo visto en clase/index.js
@@ -0,0 +1,75 @@
+/* console.log("Hola chicas") */
+
+/* Datos primitivos */
+
+    /* String
+    console.log('Hello')
+    console.log("Hello2")
+    console.log("70") 
+    */
+
+    /* Number 
+    console.log(70)
+    console.log(150800700) 
+    */
+
+    /* Boolean
+    console.log(true)
+    console.log(false) */
+
+    /* Null
+    console.log(null) */
+   
+    /* Undefined
+    console.log(undefined)
+    */
+
+
+    /* console.log(2 + 2)
+    console.log("Hello " + "chicas") */
+
+    /* 
+        typeof
+    console.log(typeof "true") */
+
+    
+    /* Variables */
+    /* var firstNumber = "Hello "
+    var secondNumber = "chicas"
+    console.log(firstNumber + secondNumber) */
+    
+
+    /* 
+    Operadores aritmeticos
+        +, -, %, /, * 
+        % = (Modulo) El resto de una division
+        
+        console.log(7 % 2)
+    
+        */
+
+    /* Operadores de asignacion
+        = 
+        var nombre = "ADA" 
+        */
+
+    /* Operadores de comparacion 
+        ==, ===, !=, !==, <, >, <= , >=
+
+        == : No hace comparacion de datos
+        ===: Si hace comparacion de datos, estricta
+    */
+    /* console.log(5 >= 5) */
+
+    /* console.log(5 == "5") */
+    /* console.log(5 == "7") */
+    /* console.log(5 !== "5") */
+
+    /* console.log("hello " + 5)
+    =======>   Convierte el 5 que esta del tipo number a tipo string */
+
+    console.log(undefined == null)
+    console.log(undefined === null)
+    console.log("Hello " + "ADA " + 2022)
+    console.log("5" + 5)
+    console.log(5 + "5")
